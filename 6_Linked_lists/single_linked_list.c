1b5ef9c0e565b44ee65a7bf53cee8fe1edd3d725
balbader <basilalbapro@gmail.com>
2024-10-01 20:30:06 +0200

    WIP

--- 6_Linked_lists/single_linked_list.c
+++ 6_Linked_lists/single_linked_list.c
@@ -3,45 +3,45 @@
 
 typedef struct node
 {
-	int	value;
-	struct node *next;
+    int	value;
+    struct node *next;
 } Node;
 
 void	print_list(Node *lst)
 {
-	Node	*current;
-	char	name;
+    Node	*current;
+    char	name;
 
-	if (!lst)
-		return ;
-	current = lst;
-	name = 'a';
-	while (current != NULL)
-	{
-		printf("Node[%c]: %d\n", name, current->value);
-		current = current->next;
-		name++;
-	}
+    if (!lst)
+        return ;
+    current = lst;
+    name = 'a';
+    while (current != NULL)
+    {
+        printf("Node[%c]: %d\n", name, current->value);
+        current = current->next;
+        name++;
+    }
 }
 
-int	main(void)
+int     main(void)
 {
-	Node	*a;
-	Node	*b;
-	Node	*c;
+    Node	*a;
+    Node	*b;
+    Node	*c;
 
-	a = (Node *)malloc(sizeof(Node));
-	b = (Node *)malloc(sizeof(Node));
-	c = (Node *)malloc(sizeof(Node));
+    a = (Node *)malloc(sizeof(Node));
+    b = (Node *)malloc(sizeof(Node));
+    c = (Node *)malloc(sizeof(Node));
 
-	a->value = 10;
-	b->value = 20;
-	c->value = 30;
+    a->value = 10;
+    b->value = 20;
+    c->value = 30;
 
-	a->next = b;
-	b->next = c;
-	c->next = NULL;
+    a->next = b;
+    b->next = c;
+    c->next = NULL;
 
-	print_list(a);
-	return (0);
+    print_list(a);
+    return (0);
 }
