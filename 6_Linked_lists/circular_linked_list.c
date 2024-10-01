1c3e682b7faaa8edfd43e8c9e05d3ce953bd280c
balbader <basilalbapro@gmail.com>
2024-10-01 20:26:52 +0200

    WIP

--- 6_Linked_lists/circular_linked_list.c
+++ 6_Linked_lists/circular_linked_list.c
@@ -3,31 +3,31 @@
 
 typedef struct s_list
 {
-	int				value;
-	struct s_list	*next;
+    int				value;
+    struct s_list	*next;
 }				t_list;
 
 int	main(void)
 {
 
-	t_list	*head;
-	t_list	*a;
-	t_list	*b;
-	t_list	*c;
+    t_list	*head;
+    t_list	*a;
+    t_list	*b;
+    t_list	*c;
 
-	a = (t_list *)malloc(sizeof(t_list) * 1);
-	b = (t_list *)malloc(sizeof(t_list) * 1);
-	c = (t_list *)malloc(sizeof(t_list) * 1);
+    a = (t_list *)malloc(sizeof(t_list) * 1);
+    b = (t_list *)malloc(sizeof(t_list) * 1);
+    c = (t_list *)malloc(sizeof(t_list) * 1);
 
-	a->value = 1;
-	b->value = 2;
-	c->value = 3;
+    a->value = 1;
+    b->value = 2;
+    c->value = 3;
 
-	a->next = b;
-	b->next = c;
-	c->next = a;
+    a->next = b;
+    b->next = c;
+    c->next = a;
 
-	/* Save address of c node in head */
-	head = a;
-	return (0);
+    /* Save address of c node in head */
+    head = a;
+    return (0);
 }
